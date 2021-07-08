e6148ffb4613e2eecc106f11c7171b03b9e0f34a
Philogene Kyle Dimpas <dimpaspk@ktdp.local>
2021-07-08 17:08:47 +0800

    Added Header, Footer, and bootstrap css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
+import { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div className="App">
-    </div>
+    <Fragment>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1> Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </Fragment>
   );
-}
+};
 
 export default App;
