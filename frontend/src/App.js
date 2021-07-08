9922083b88c85f10be499e910a380057ca018144
Philogene Kyle Dimpas <dimpaspk@ktdp.local>
2021-07-08 19:55:23 +0800

    Implement react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import { Fragment } from 'react';
+import { Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 const App = () => {
   return (
     <Fragment>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
