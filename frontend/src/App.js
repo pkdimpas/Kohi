0934e7f337ff84bb979f8bf4b228a8ad38350b11
Philogene Kyle Dimpas <dimpaspk@ktdp.local>
2021-07-08 20:38:03 +0800

    Add Product Screen. Removed import React from 'react' in all jsx files.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react';
 import { Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
