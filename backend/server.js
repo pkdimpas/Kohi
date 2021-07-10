804f4d4f73772b5ab6821f1e6af0840d76b2875f
pkdimpas <pkdimpas@gmail.com>
2021-07-10 14:55:04 +0800

    update backend to ES module

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import { config } from 'dotenv';
+import products from './data/products.js';
+
+config();
+
 const app = express();
 
 app.get('/', (req, res) => {
@@ -10,8 +14,14 @@ app.get('/api/products/', (req, res) => {
 });
 
 app.get('/api/products/:id', (req, res) => {
-  const product = products.find((product) => product._id === req.params.id);
+  const product = find((product) => product._id === req.params.id);
 
   res.json(product);
 });
-app.listen(5000, console.log('Server running on port 5000'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
