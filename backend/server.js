5dbd472bbacdcfb208be4fed5c92f7796402fd7d
pkdimpas <pkdimpas@gmail.com>
2021-07-11 16:55:42 +0800

    Connecting to DB using Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 import express from 'express';
-import { config } from 'dotenv';
+import dontenv from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
-config();
+dontenv.config();
+
+connectDB();
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('API is running');
 });
+
 app.get('/api/products/', (req, res) => {
   res.json(products);
 });
