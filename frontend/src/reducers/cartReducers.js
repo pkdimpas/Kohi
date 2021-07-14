c508ed66e2c631525a20a8df4996b82c2b0be223
Philogene Kyle Dimpas <dimpaspk@ktdp.local>
2021-07-14 17:40:20 +0800

    CartScreen with add to cart functionality

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,28 @@
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const currentItem = action.payload;
+
+      const existItem = state.cartItems.find(
+        (item) => item.product === currentItem.product
+      );
+
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((item) =>
+            item.product === existItem.product ? currentItem : item
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, currentItem],
+        };
+      }
+    default:
+      return state;
+  }
+};
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,10 +1,120 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link, useHistory, useLocation, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+//components
+import { Row, Col, ListGroup } from 'react-bootstrap';
+import { Image, Form, Button, Card } from 'react-bootstrap';
+import Message from '../components/Message';
+//actions
+import { addToCart } from '../actions/cartActions';
 
 const CartScreen = () => {
+  const { id: productID } = useParams();
+  const { search } = useLocation();
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const qty = Number(new URLSearchParams(search).get('qty'));
+
+  const { cartItems } = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    if (productID) {
+      dispatch(addToCart(productID, qty));
+    }
+  }, [dispatch, productID, qty]);
+
+  const removeFromCartHandler = (id) => {
+    console.log('remove ' + id);
+  };
+
+  const checkoutHandler = () => {
+    history.push('/login?redirect=shipping');
+  };
+
   return (
-    <div>
-      <p>Cart</p>
-    </div>
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 && (
+          <Message>
+            Your Cart is empty <Link to='/'>Go Back</Link>
+          </Message>
+        )}
+
+        {cartItems.length !== 0 && (
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+
+                  <Col md={2}>${item.price}</Col>
+
+                  <Col md={2}>
+                    <Form.Control
+                      as='select'
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((value) => (
+                        <option key={value + 1} value={value + 1}>
+                          {value + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+
+                  <Col md={2}>
+                    <Button
+                      variant='light'
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className='fas fa-trash'></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items
+              </h2>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                blocked
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
+              >
+                Proceed to Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
