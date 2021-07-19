e7717515beefe42005118427fe19569bfedd9a5c
Philogene Kyle Dimpas <pkdimpas@gmail.com>
2021-07-19 17:57:24 +0800

    Payment Screen & Payment Method

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
+  CART_SAVE_PAYMENT_METHOD,
   CART_SAVE_SHIPPING_ADDRESS,
 } from '../constants/cartConstants';
 
@@ -44,4 +45,13 @@ export const saveShippingAddress = (data) => (dispatch) => {
   });
 
   localStorage.setItem('shippingAddress', JSON.stringify(data));
+};
+
+export const savePaymentMethod = (payment_method) => (dispatch) => {
+  dispatch({
+    type: CART_SAVE_PAYMENT_METHOD,
+    payload: payment_method,
+  });
+
+  localStorage.setItem('paymentMethod', JSON.stringify(payment_method));
 };
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,6 +1,7 @@
 import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
+  CART_SAVE_PAYMENT_METHOD,
   CART_SAVE_SHIPPING_ADDRESS,
 } from '../constants/cartConstants';
 
@@ -29,6 +30,7 @@ export const cartReducer = (
           cartItems: [...state.cartItems, currentItem],
         };
       }
+
     case CART_REMOVE_ITEM:
       return {
         ...state,
@@ -36,8 +38,13 @@ export const cartReducer = (
           (item) => item.product !== action.payload
         ),
       };
+
     case CART_SAVE_SHIPPING_ADDRESS:
       return { ...state, shippingAddress: action.payload };
+
+    case CART_SAVE_PAYMENT_METHOD:
+      return { ...state, paymentMethod: action.payload };
+
     default:
       return state;
   }
