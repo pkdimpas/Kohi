3cea76fe589298e0f79755af7bf12999abd8c0fc
Philogene Kyle Dimpas <pkdimpas@gmail.com>
2021-07-19 20:27:26 +0800

    Order Route and temp Controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -90,6 +90,7 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   if (user) {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
+    //user.isAdmin = req.body.isAdmin ?? user.isAdmin
 
     if (req.body.password) {
       user.password = req.body.password;
