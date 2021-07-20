9ee480c9856e0aacba99716845da41d716af8dc0
pkdimpas <pkdimpas@gmail.com>
2021-07-20 15:52:36 +0800

    Get Order by ID Endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -4,7 +4,7 @@ import Order from '../models/orderModel.js';
 /**
  * @desc    Create new order
  * @route   POST /api/orders
- * @access  Public
+ * @access  Private
  */
 const addOrderItems = asyncHandler(async (req, res) => {
   const {
@@ -38,4 +38,23 @@ const addOrderItems = asyncHandler(async (req, res) => {
   res.status(201).json(createdOrder);
 });
 
-export { addOrderItems };
+/**
+ * @desc    Get order by ID
+ * @route   GET /api/orders/:id
+ * @access  Private
+ */
+const getOrderById = asyncHandler(async (req, res) => {
+  //populate from user with name and email fields.
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+export { addOrderItems, getOrderById };
