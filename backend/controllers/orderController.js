89efac392c964d7fc90b07ca36f0b7c8e7ef3e76
Philogene Kyle Dimpas <pkdimpas@gmail.com>
2021-07-21 20:58:52 +0800

    backend for show orders in profile

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -85,4 +85,14 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid };
+/**
+ * @desc    GET logged in user orders
+ * @route   GET /api/orders/myorders
+ * @access  Private
+ */
+const getMyOrders = asyncHandler(async (req, res) => {
+  //populate from user with name and email fields.
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
+});
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
